b361e84adb32ed071959d350d4eede14d33cef1a
jakobbehler <80776480+jakobbehler@users.noreply.github.com>
2025-09-18 16:54:07 +0200

    Add files via upload

---
+++ 3_helpers_from_class.py
@@ -0,0 +1,20 @@
+
+'''
+Linear regression using gradient descent
+mean squared error gd(y, tx, initial w, max iters, gamma)
+
+Linear regression using stochastic gradient descent
+mean squared error sgd(y, tx, initial w, max iters, gamma)
+
+Least squares regression using normal equations
+least squares(y, tx)
+
+Ridge regression using normal equations
+ridge regression(y, tx, lambda )
+
+Logistic regression using gradient descent (y∈ {0,1})
+logistic regression(y, tx, initial w,max iters, gamma)
+
+Regularized logistic regression using gradient descent (y∈ {0,1}, with regularization term λ∥w∥2
+reg logistic regression(y, tx, lambda , initial w, max iters, gamma)
+'''
